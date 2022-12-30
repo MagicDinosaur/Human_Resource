91cc0f24edb7810f92ed0dfa2d32ed3cdac23994
MagicDinosaur <phamvduc2112@gmail.com>
2022-12-29 20:26:08 -0500

    fix: User models

--- Backend/HRM/posts/models.py
+++ Backend/HRM/posts/models.py
@@ -62,31 +62,31 @@ class EmployeeLogin(models.Model):
     employee_password = models.CharField(max_length=400)
 
 
-class Authority(models.Model):
-    AUTHORITY_CODE  = [
-    ('EMPLOYEE', (
-            ('E1', 'REMOVE_EMPLOYEE'),
-            ('E2', 'ADD_EMPLOYEE'),
-            ('E3', 'UPDATE_EMPLOYEE'),
-            ('E4', 'VIEW_EMPLOYEE'),
-        )
-    ),
-    ('TEAM', (
-            ('T1', 'REMOVE_TEAM'),
-            ('T2', 'ADD_TEAM'),
-            ('T3', 'UPDATE_TEAM'),
-            ('T4', 'VIEW_TEAM'),
-        )
-    ),
-    ('PROJECT', (
-            ('P1', 'REMOVE_PROJECT'),
-            ('P2', 'ADD_PROJECT'),
-            ('P3', 'UPDATE_PROJECT'),
-            ('P4', 'VIEW_PROJECT'),
-    )),
-    ('unknown', 'Unknown')
-]
-    employee_id = models.ForeignKey(Employee, on_delete=models.CASCADE, db_column='employee_id')
-    employee_authority = models.CharField(max_length=50, choices=tuple((k,v) for k,v in AUTHORITY_CODE))
-    def __str__(self):
-        return '%s - %s' % (self.employee_id, self.employee_authority[:][:])
+# class Authority(models.Model):
+#     AUTHORITY_CODE  = [
+#     ('EMPLOYEE', (
+#             ('E1', 'REMOVE_EMPLOYEE'),
+#             ('E2', 'ADD_EMPLOYEE'),
+#             ('E3', 'UPDATE_EMPLOYEE'),
+#             ('E4', 'VIEW_EMPLOYEE'),
+#         )
+#     ),
+#     ('TEAM', (
+#             ('T1', 'REMOVE_TEAM'),
+#             ('T2', 'ADD_TEAM'),
+#             ('T3', 'UPDATE_TEAM'),
+#             ('T4', 'VIEW_TEAM'),
+#         )
+#     ),
+#     ('PROJECT', (
+#             ('P1', 'REMOVE_PROJECT'),
+#             ('P2', 'ADD_PROJECT'),
+#             ('P3', 'UPDATE_PROJECT'),
+#             ('P4', 'VIEW_PROJECT'),
+#     )),
+#     ('unknown', 'Unknown')
+# ]
+#     employee_id = models.ForeignKey(Employee, on_delete=models.CASCADE, db_column='employee_id')
+#     employee_authority = models.CharField(max_length=50, choices=tuple((k,v) for k,v in AUTHORITY_CODE))
+#     def __str__(self):
+#         return '%s - %s' % (self.employee_id, self.employee_authority[:][:])
