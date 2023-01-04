15ab54133183de4eb66e610984b38af5423ee783
MagicDinosaur <phamvduc2112@gmail.com>
2023-01-04 00:16:32 -0500

    fix: employee view

--- Backend/HRM/HRM/settings.py
+++ Backend/HRM/HRM/settings.py
@@ -103,7 +103,7 @@ DATABASES = {
 #         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
 #     },
 # ]
-
+CSRF_TRUSTED_ORIGINS=['http://127.0.0.1:8000']
 LOGIN_URL = '/admin/'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
